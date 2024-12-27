f3d585c318594da989e019e5159fe4f8379f8e44
Dean Fox <deanfoxcd@gmail.com>
2024-12-26 21:57:57 -0500

    add style Rows

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/Heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.main`
   padding: 20px;
@@ -13,12 +14,23 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as='h1'>The Wild Oasis</Heading>
-        <Heading as='h2'>The Wild Oasis</Heading>
-        <Heading as='h3'>The Wild Oasis</Heading>
-        <Button>Check in</Button>
-        <Button>Check out</Button>
-        <Input type='number' placeholder='No. of guests' />
+        <Row>
+          <Row type='horizontal'>
+            <Heading as='h1'>The Wild Oasis</Heading>
+            <div>
+              <Heading as='h2'>Check in/out</Heading>
+              <Button>Check in</Button>
+              <Button>Check out</Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as='h3'>The Wild Oasis</Heading>
+            <form>
+              <Input type='number' placeholder='No. of guests' />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
