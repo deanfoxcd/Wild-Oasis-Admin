65b7cf2980c7b46ea25641a1989cb1952599dfef
Dean Fox <deanfoxcd@gmail.com>
2024-12-26 21:48:12 -0500

    add styling different headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,25 @@
 import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+import Heading from './ui/Heading';
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
+const StyledApp = styled.main`
+  padding: 20px;
 `;
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <div>
-        <H1>The Wild Oasis</H1>
+      <StyledApp>
+        <Heading as='h1'>The Wild Oasis</Heading>
+        <Heading as='h2'>The Wild Oasis</Heading>
+        <Heading as='h3'>The Wild Oasis</Heading>
         <Button>Check in</Button>
         <Button>Check out</Button>
         <Input type='number' placeholder='No. of guests' />
-      </div>
+      </StyledApp>
     </>
   );
 }
