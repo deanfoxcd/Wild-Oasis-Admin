e10d00c242fc82d4dc8fb061b4b397dfb20945aa
Dean Fox <deanfoxcd@gmail.com>
2025-01-06 16:11:01 -0500

    add modal window for add/edit cabins

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -4,7 +4,6 @@ import Input from '../../ui/Input';
 import useSettings from './useSettings';
 import Spinner from '../../ui/Spinner';
 import useEditSettings from './useEditSettings';
-import { useForm } from 'react-hook-form';
 
 export default function UpdateSettingsForm() {
   const {
