75548eb2f03dd08b8948a0ae965648d0daa561ce
Dean Fox <deanfoxcd@gmail.com>
2025-01-07 07:26:05 -0500

    add sort button and options

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from '../../ui/TableOperations';
 import Filter from '../../ui/Filter';
+import SortBy from '../../ui/SortBy';
 
 function CabinTableOperations() {
   return (
@@ -10,6 +11,22 @@ function CabinTableOperations() {
           { value: 'all', label: 'All' },
           { value: 'no-discount', label: 'No Discount' },
           { value: 'with-discount', label: 'With Discount' },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: 'name-asc', label: 'Sort by name (A-Z)' },
+          { value: 'name-dsc', label: 'Sort by name (Z-A)' },
+          { value: 'regularPrice-asc', label: 'Sort by price (high to low)' },
+          { value: 'regularPrice-dsc', label: 'Sort by price (low to high)' },
+          {
+            value: 'maxCapacity-asc',
+            label: 'Sort by max capacity (low to high)',
+          },
+          {
+            value: 'maxCapacity-dsc',
+            label: 'Sort by max capacity (high to low)',
+          },
         ]}
       />
     </TableOperations>
