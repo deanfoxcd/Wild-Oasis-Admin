e925d8dbd2c6ba12384ccebc98a61829a6fe5f46
Dean Fox <deanfoxcd@gmail.com>
2025-01-11 21:54:27 -0500

    add recent bookings and stays for last 7, 30, 90 days (not displayed yet)

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import useRecentBookings from './useRecentBookings';
+import useRecentStays from './useRecentStays';
+import Spinner from '../../ui/Spinner';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,15 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+  console.log(confirmedStays);
+
+  return <StyledDashboardLayout></StyledDashboardLayout>;
+}
+
+export default DashboardLayout;
