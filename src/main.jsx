875ba7989f871e1eaf6e369cbba80908d8dec832
Dean Fox <deanfoxcd@gmail.com>
2025-01-13 11:44:34 -0500

    add error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { StyleSheetManager } from 'styled-components';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import ErrorFallback from './ui/ErrorFallback';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StyleSheetManager shouldForwardProp={() => true}>
-      <App />
-    </StyleSheetManager>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace('/')}
+    >
+      <StyleSheetManager shouldForwardProp={() => true}>
+        <App />
+      </StyleSheetManager>
+    </ErrorBoundary>
   </React.StrictMode>
 );
